fcc96c49df0b2316ebd90a25ec80d53e48bf8ead
김하민 <khmin1017@gmail.com>
2021-05-26 14:28:03 +0900

    상품 디테일 페이지 구현(1)
    - 수정사항 반영(상품image 불러오기, 리턴메시지 대문자 수정)
    - unittest 작성
    - unittest tearDown 수정

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,87 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from .models       import Collection, Product, ProductOption, ProductImage, Brand
+from orders.models import SellingInformation, BuyingInformation, Status, Order
+from users.models  import User
+from django.test   import TestCase
+from django.test   import Client
+
+class ProductTest(TestCase):
+    def setUp(self):
+        user1      = User.objects.create(social_login_id = "noogoo1982", nickname="누구쉰지", email="noogoo1982@gmail.com", point=0)
+        user2      = User.objects.create(social_login_id = "noogoo1983", nickname="누구쒼지", email="noogoo1983@gmail.com", point=0)
+        brand      = Brand.objects.create(name="나이키", logo_image="aa")
+        collection = Collection.objects.create(name="조던조던조던", brand = brand)
+        product    = Product.objects.create(id=1,korean_name = "조던", english_name="jordan", limited=True, collection = collection)
+        
+        ProductImage.objects.create(product = product, image_url = "a")
+        ProductImage.objects.create(product = product, image_url = "b")
+        
+        status         = Status.objects.create(name="입찰대기")
+        product_option = ProductOption.objects.create(product = product, size = "250")
+
+        SellingInformation.objects.create(
+            price          = 240000,
+            status         = status,
+            user           = user1,
+            product_option = product_option)
+        
+        SellingInformation.objects.create(
+            price          = 340000,
+            status         = status,
+            user           = user1,
+            product_option = product_option
+        )
+
+        BuyingInformation.objects.create(
+            price          = 200000,
+            status         = status,
+            user           = user2,
+            product_option = product_option
+        )
+
+        BuyingInformation.objects.create(
+            price          = 190000,
+            status         = status,
+            user           = user2,
+            product_option = product_option
+        )
+
+    def tearDown(self):
+        Brand.objects.all().delete()
+        Collection.objects.all().delete()
+        Product.objects.all().delete()
+        ProductOption.objects.all().delete()
+        ProductImage.objects.all().delete()
+        Status.objects.all().delete()
+        SellingInformation.objects.all().delete()
+        BuyingInformation.objects.all().delete()
+
+    def test_productdetail_get_success(self):
+        client   = Client()
+        response = client.get('/products/1')
+
+        self.assertEqual(response.json(),{
+            "product_information" : {
+                "id"           : 1,
+                "english_name" : "jordan",
+                "korean_name"  : "조던",
+                "main_image"   : "a",
+                "sub_image"    : "b",
+                "option" : [
+                    {
+                        "size"       : "250",
+                        "buy_price"  : "240000.0000",
+                        "sell_price" : "200000.0000"
+                    }
+                ],
+                "lately_order" : None,
+            }
+        })
+        self.assertEqual(response.status_code, 200)
+
+    def test_productdetail_get_no_product(self):
+        client   = Client()
+        response = client.get('/products/2')
+        
+        self.assertEqual(response.status_code, 400)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.db.models.aggregates import Max, Min
+from django.db.models            import Q
+from django.http                 import JsonResponse
+from django.views                import View
+
+from products.models import Product, ProductOption, ProductImage
+from orders.models   import SellingInformation, BuyingInformation, Status, Order
+
+
+class ProductDetailView(View):
+    def get(self, request, product_id):
+        if Product.objects.filter(id = product_id).exists():
+            product         = Product.objects.get(id = product_id)
+            product_image   = product.productimage_set.all()
+            product_options = ProductOption.objects.filter(product = product)
+            bidding         = Status.objects.get(name="입찰대기")
+            q               = Q(selling_information__product_option__product=product)
+
+            option = [
+                {
+                    'size'       : product_option.size,
+                    'buy_price'  : SellingInformation.objects.filter(product_option = product_option, status = bidding).aggregate(Min("price"))["price__min"],
+                    "sell_price" : BuyingInformation.objects.filter(product_option = product_option, status = bidding).aggregate(Max("price"))["price__max"],
+                    } 
+                    for product_option in product_options
+                    ]
+            
+            lately_order = None
+
+            if Order.objects.filter(q).exists():
+                lately_order = Order.objects.filter(q).order_by("create_at")[0].selling_information.price
+
+            product_information = {
+                "id"           : product.id,
+                "english_name" : product.english_name,
+                "korean_name"  : product.korean_name,
+                "main_image"   : product_image[0].image_url,
+                "sub_image"    : product_image[1].image_url,
+                "option"       : option,
+                'lately_order' : lately_order,
+                }
+
+            return JsonResponse({"product_information" : product_information}, status=200)
+        
+        return JsonResponse({"MESSAGE" : "NO_PRODUCT"}, status = 400)
