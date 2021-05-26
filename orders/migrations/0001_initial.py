3fa9ba00b43c7477436221aefe804a77ab8590ea
hwaya2828 <hwaya2828@gmail.com>
2021-05-26 21:41:33 +0900

    ADD: 소셜 로그인
    - 카카오톡 연동 소셜 로그인 기능 구현
    - httpie 통해 결과 확인
    - 유닛 테스트 첨부
    
    - 리뷰 사항 반영
        - 리뷰 주신 메소드 사용해서 코드 개선
        - .gitignore 적용 파일 추가
        - 로그인 시 발생할 수 있는 에러와 그에 따른 유닛 테스트 추가

--- orders/migrations/0001_initial.py
+++ orders/migrations/0001_initial.py
@@ -9,8 +9,8 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
+        ('products', '__first__'),
         ('users', '0001_initial'),
-        ('products', '0001_initial'),
     ]
 
     operations = [
