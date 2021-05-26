106b7026c2d1099cc1c4f49801283779e9043fac
hwaya2828 <hwaya2828@gmail.com>
2021-05-26 16:25:05 +0900

    MODIFY: 모델링 변경 - 소셜 로그인 구현 위해 USERS TABLE COLUMN 변경 - 대체적인 MAX_DIGITS 속성 상향 수정

--- users/models.py
+++ users/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class User(models.Model):
-    nickname  = models.CharField(max_length=45)
-    email     = models.CharField(max_length=45, unique=True)
-    password  = models.CharField(max_length=200)
-    point     = models.IntegerField(default=0)
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    social_login_id = models.CharField(max_length=200, unique=True)
+    nickname        = models.CharField(max_length=45, null=True)
+    email           = models.EmailField(max_length=200, null=True)
+    point           = models.IntegerField(default=0)
+    create_at       = models.DateTimeField(auto_now_add=True)
+    update_at       = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = "users"
