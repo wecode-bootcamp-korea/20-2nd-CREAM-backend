0d7c7d2c4de8c5799c6cfcac5a663dd7d5dfd2be
hwaya2828 <hwaya2828@gmail.com>
2021-05-28 18:36:13 +0900

    ADD: 조건별 상품 리스트 전송 기능 추가
    - 특정 브랜드 상품
    - 특정 컬렉션 상품
    - 필터링 (조건 3가지 : 사이즈, 색상, 한정판)
    - 검색
    - 유닛 테스트 추가
    
    - 리뷰 사항 반영
        - prefetch_related() 메소드 사용
        - setUpTestData 메소드 사용

--- products/views.py
+++ products/views.py
@@ -5,9 +5,81 @@ from django.db.models            import Q
 from django.http                 import JsonResponse
 from django.views                import View
 
-from products.models import Product, ProductOption, ProductImage
-from orders.models   import SellingInformation, BuyingInformation, Status, Order
+from products.models             import Product, ProductOption
+from orders.models               import SellingInformation, BuyingInformation, Status, Order
 
+class ProductListView(View):
+    def get(self, request):
+        brand_id      = request.GET.getlist('brand_id', None)         # 브랜드별 상품 리스트
+        collection_id = request.GET.getlist('collection_id', None)    # 컬렉션별 상품 리스트
+        size          = request.GET.getlist('size', None)             # 필터링 조건1 - 사이즈
+        colors        = request.GET.getlist('color', None)            # 필터링 조건2 - 색상 (상품명 안에 포함되어 있음)
+        limited       = request.GET.get('limited', None)              # 필터링 조건3 - 한정판 유무
+        search        = request.GET.get('search', None)               # 검색 - 상품명 기준
+
+        q = Q()
+
+        if brand_id:
+            q.add(Q(collection__brand_id__in=brand_id), q.AND)
+        
+        if collection_id:
+            q.add(Q(collection_id__in=collection_id), q.AND)
+
+        if colors:
+            q_coler = Q()
+            for color in colors:
+                q_coler.add(Q(english_name__contains=color), q.OR)
+            q.add(q_coler, q.AND)
+        
+        if limited:
+            q.add(Q(limited=limited), q.AND)
+        
+        if search:
+            search = (search.lower()).capitalize()
+            q.add(Q(korean_name__contains=search) | Q(english_name__contains=search), q.AND)
+
+        products = Product.objects.select_related('collection', 'collection__brand').prefetch_related(
+            'productoption_set',
+            'productoption_set__sellinginformation_set',
+            'productoption_set__buyinginformation_set'
+        ).filter(q).distinct()
+
+        total_result = []
+
+        for product in products:
+            q_size = Q()
+            if size:
+                q_size.add(Q(size__in=size), q.AND)
+            
+            result = [
+                {
+                    "brand_information"    : {
+                                                "brand_id"       : product.collection.brand.id,
+                                                "brand_name"     : product.collection.brand.name,
+                                                "brand_logo_url" : product.collection.brand.logo_image
+                    },
+                    "collection_id"        : product.collection.id,
+                    "collection_name"      : product.collection.name,
+                    "product_id"           : product.id,
+                    "product_korean_name"  : product.korean_name,
+                    "product_english_name" : product.english_name,
+                    "product_limited"      : product.limited,
+                    "product_main_image"   : product.productimage_set.all()[0].image_url,
+                    "product_options"      : [
+                                                {
+                                                    "size"       : option.size,
+                                                    "buy_price"  : option.sellinginformation_set.filter(product_option_id=option.id).all().order_by('price')[0].price if option.sellinginformation_set.filter(product_option_id=option.id).exists() else None,
+                                                    "sell_price" : option.buyinginformation_set.filter(product_option_id=option.id).all().order_by('-price')[0].price if option.buyinginformation_set.filter(product_option_id=option.id).exists() else None
+                                                } for option in product.productoption_set.filter(q_size)
+                                            ]
+
+                }
+            ]
+            
+            total_result.append(result)
+        
+        return JsonResponse({'result': total_result}, status=200)
+        
 class ProductDetailView(View):
     def get(self, request, product_id):
         if Product.objects.filter(id = product_id).exists():
