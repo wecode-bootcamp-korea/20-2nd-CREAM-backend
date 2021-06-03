49f7ae483e3e01a0328ab0426c14abaa462775bf
김하민 <khmin1017@gmail.com>
2021-06-03 12:57:18 +0900

    conflict 해결

--- orders/views.py
+++ orders/views.py
@@ -39,3 +39,34 @@ class BuyView(View):
         }
 
         return JsonResponse({"product_information" : product_information}, status=200)
+
+class SellView(View):
+    @login_confirm
+    def get(self, request, product_id):
+        user           = request.user
+        size           = request.GET.get("size", None)
+        status         = Status.objects.get(name = "입찰대기")
+        product        = Product.objects.prefetch_related('productimage_set', 'productoption_set').get(id=product_id)
+        product_option = product.productoption_set.get(size=size)
+        buying         = BuyingInformation.objects.filter(Q(product_option = product_option, status = status))
+        
+        selling_price  = buying.aggregate(Max("price"))["price__max"] if buying.exists() else None
+        buying_user    = buying.filter(price = selling_price).order_by("-create_at")[0].user.id if buying.exists() else None
+        buying_id      = buying.filter(price = selling_price).order_by("-create_at")[0].id if buying.exists() else None
+        
+        buying_price   = SellingInformation.objects.filter(product_option = product_option, status = status).aggregate(Min("price"))["price__min"]\
+                         if SellingInformation.objects.filter(product_option = product_option, status = status).exists() else None
+            
+        product_information = {
+            "korean_name"   : product.korean_name,
+            "english_name"  : product.english_name,
+            "image"         : product.productimage_set.all()[0].image_url,
+            "selling_price" : selling_price,
+            "buying_price"  : buying_price,
+            "buying_user"   : buying_user,
+            "buying_id"     : buying_id,
+            "user_point"    : user.point,
+            "size"          : size
+        }
+
+        return JsonResponse({"product_information" : product_information}, status=200)
